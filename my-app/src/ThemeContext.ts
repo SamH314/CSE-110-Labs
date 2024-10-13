5a71197461470bf76d6d1b4cb96426429dccdb9b
SamH314 <samuelhernandez010904@gmail.com>
2024-10-13 16:50:04 -0700

    FINSIHED THE LAB

--- my-app/src/ThemeContext.ts
+++ my-app/src/ThemeContext.ts
@@ -1,3 +1,4 @@
+// ThemeContext.ts
 import React from 'react';
 
 export const themes = {
@@ -12,3 +13,4 @@ export const themes = {
 };
 
 export const ThemeContext = React.createContext(themes.light);
+
--- my-app/src/hooksExercise.tsx
+++ my-app/src/hooksExercise.tsx
@@ -1,22 +1,77 @@
+import { useContext } from 'react';
+import { ThemeContext, themes } from "./ThemeContext";
 import React, { useState, useEffect } from 'react';
 
-export function ClickCounter() {
- const [count, setCount] = useState(0);
 
+
+export function ClickCounter() {
+    {/* 
+        -creates variable count, with setcount as the function that updates it
+        -initializes count to be 0 
+        -useState is a react hook that declares the above^
+        */}
+ const [count, setCount] = useState(0);
+        //handleClick is a function that increments count, using setCount function
  const handleClick = () => {
    setCount(count + 1);
  };
 
+    {/*
+        -useEffect is a reacthook 
+        -updates the name of the document every time count is incremented 
+        */}
  useEffect(() => {
    document.title = `You clicked ${count} times`;
- }, [count]);
+ }, [count]);//dependency array: works like the trigger to a trap, runs when count changes
 
- return (
-   <div>
-     <p>Clicks: {count}</p>
-     <button onClick={handleClick}>Click me!</button>
+    //useContext hook is used to access theme-related styles from ThemeContext for styling 
+    const theme = useContext(ThemeContext);
+
+    //renders everything/kind of like the styling 
+return (
+   <div
+    style={{
+     background: theme.background,
+       color: theme.foreground,
+       padding: "20px",
+     }}
+   >
+     <p>You clicked {count} times </p>
+     <button
+       onClick={() => setCount(count + 1)} //incrememnts count by 1 when clicked 
+       style={{ background: theme.foreground, color: theme.background }}
+     >
+       Click me
+     </button>
    </div>
  );
 }
+
+
+
+  function ToggleTheme() {
+    {/*
+        -useState is a react hook that does the below things:
+        -initializes the theme to light 
+        -creates the variable currentTheme and the function setCurrentTheme which updates currentTheme
+        */}
+    const [currentTheme, setCurrentTheme] = useState(themes.light);
+
+    //fucntion that changes the current theme to the opposite of the current theme
+    const toggleTheme = () => {
+      setCurrentTheme(currentTheme === themes.light ? themes.dark : themes.light);
+    };
   
-  export default ClickCounter;
+    //basically renders everything 
+    {/*
+        provides context to ClickCounter, specifically currentTheme
+        */}
+
+    return (
+      <ThemeContext.Provider value={currentTheme}>
+        <button onClick={toggleTheme}> Toggle Theme </button>
+        <ClickCounter />
+      </ThemeContext.Provider>
+    );
+   }
+   export default ToggleTheme;
